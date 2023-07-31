1e5f49ff283419b1ccb22c453016659e9dea2dc1
chanyda <qkrcodms99@gmail.com>
2023-07-31 15:17:00 +0900

    init, associate 로직 변경

--- ch9/models/index.ts
+++ ch9/models/index.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 import { dbConfig } from "../config/dbConfig";
-import path from "path";
-import fs from "fs";
+import User from "./user";
+import Post from "./post";
+import Hashtag from "./hashtag";
 
 const env = process.env.NODE_ENV || "development";
 const { database, username, password } = dbConfig[env];
@@ -10,19 +11,12 @@ let db;
 const sequelize = new Sequelize(database, username, password, dbConfig[env]);
 db = { sequelize };
 
-const basename = path.basename(__filename); // index.ts 현재 파일
-fs.readdirSync(__dirname)
-    .filter((file) => file.indexOf(".") !== 0 && file !== basename)
-    .forEach((file) => {
-        const model = require(path.join(__dirname, file));
-        db[model.default] = model;
-        model.default.initiate(sequelize);
-    });
+User.initiate(sequelize);
+Post.initiate(sequelize);
+Hashtag.initiate(sequelize);
 
-Object.keys(db).forEach((modelName: string) => {
-    if (db[modelName].associate) {
-        db[modelName].associate();
-    }
-});
+User.associate();
+Post.associate();
+Hashtag.associate();
 
 export default db;
